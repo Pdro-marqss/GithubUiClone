8ee427f78870653157658a606ccea5c16c19ae82
Pedro Marques <pdro.marqs2015@hotmail.com>
2022-07-20 23:21:12 -0300

    calendar created + Profile adjusts

---
+++ src/components/RandomCalendar/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Heatmap from 'react-calendar-heatmap';
+import { subYears } from 'date-fns';
+
+import { Container } from './styles';
+
+type HeatmapValue = { date: Date; count: number; }
+
+
+const RandomCalendar: React.FC = () => {
+    const startDate = subYears(new Date(), 1);
+    const endDate = new Date();
+
+    const values: HeatmapValue[] = [];
+
+    values.push({ date: new Date(), count: 3 });
+
+    return (
+        <Container>
+            <div className='wrapper'>
+                <Heatmap
+                    startDate={startDate}
+                    endDate={endDate}
+                    values={values}
+                    gutterSize={3.5}
+                    classForValue={(item: HeatmapValue) => {
+                        let clampedCount = 0;
+
+                        if (item !== null) {
+                            clampedCount = Math.max(item.count, 0);
+                            clampedCount = Math.min(item.count, 4);
+                        }
+
+                        return `scale-${clampedCount}`
+                    }}
+                    showWeekdayLabels
+                />
+            </div>
+
+            <span>Random calendar (do not represent actual data)</span>
+        </Container>
+    );
+};
+
+export default RandomCalendar;
+
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import { Container, Main, LeftSide, RightSide, Repos } from './styles';
+import { Container, Main, LeftSide, RightSide, Repos, CalendarHeading } from './styles';
 
 import ProfileData from '../../components/ProfileData';
 import RepoCard from '../../components/RepoCard';
+import RandomCalendar from '../../components/RandomCalendar';
 
 const Profile: React.FC = () => {
     return (
@@ -41,6 +42,12 @@ const Profile: React.FC = () => {
                             ))}
                         </div>
                     </Repos>
+
+                    <CalendarHeading>
+                        Random calendar (do not represent actual data)
+                    </CalendarHeading>
+
+                    <RandomCalendar />
                 </RightSide>
             </Main>
         </Container>
